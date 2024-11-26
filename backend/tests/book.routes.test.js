4799873dcaafbbcbc658c83f0945636be1a48bd0
Loading-Yann <yann.jadeau@gmail.com>
2024-11-26 20:49:47 +0100

    correction de la suppression d'images lors des test

--- backend/tests/book.routes.test.js
+++ backend/tests/book.routes.test.js
@@ -9,19 +9,31 @@ beforeAll(async () => {
   await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 });
 
-// Nettoyer la base après chaque test
+// Nettoyer la base et supprimer les fichiers générés après chaque test
 afterEach(async () => {
+  // Suppression des livres dans la base de données
   await Book.deleteMany({});
-  const testFilePath = './images/sample.jpg_1732629948964-optimized.webp';
-  if (fs.existsSync(testFilePath)) {
-    fs.unlinkSync(testFilePath);
-  }
+
+  // Recherche et suppression des fichiers générés par les tests
+  const imageFolder = './images';
+  const files = fs.readdirSync(imageFolder);
+
+  files.forEach((file) => {
+    if (file.startsWith('sample')) {
+      const filePath = `${imageFolder}/${file}`;
+      try {
+        fs.unlinkSync(filePath);
+      } catch (error) {
+        console.error(`Erreur lors de la suppression du fichier ${filePath} : ${error.message}`);
+      }
+    }
+  });
 });
 
 // Déconnexion après les tests
 afterAll(async () => {
   await mongoose.disconnect();
-  await new Promise(resolve => setTimeout(resolve, 1000)); // Délai pour fermer proprement les connexions
+  await new Promise((resolve) => setTimeout(resolve, 1000)); // Délai pour fermer proprement les connexions
 });
 
 describe('Tests pour les routes /api/books', () => {
@@ -36,12 +48,15 @@ describe('Tests pour les routes /api/books', () => {
       .post('/api/books')
       .set('Authorization', `Bearer ${process.env.TEST_USER_TOKEN}`) // Token valide
       .attach('image', 'tests/sample.jpg') // Ajout d'un fichier image
-      .field('book', JSON.stringify({ 
-        title: 'Test Book', 
-        author: 'Test Author', 
-        year: 2020, 
-        genre: 'Test Genre' 
-      }));
+      .field(
+        'book',
+        JSON.stringify({
+          title: 'Test Book',
+          author: 'Test Author',
+          year: 2020,
+          genre: 'Test Genre',
+        })
+      );
 
     expect(response.status).toBe(201);
     expect(response.body.message).toBe('Livre ajouté avec succès.');
